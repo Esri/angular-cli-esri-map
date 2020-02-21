256a41215fb020e4f961225d5b9a33387afe477c
Andy <agup2001@msn.com>
2020-02-21 16:28:33 -0700

    Remove esri types

--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -12,7 +12,6 @@
 */
 
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Input, Output, EventEmitter } from '@angular/core';
-import esri = __esri; // Esri TypeScript Types
 import Map from "esri/Map";
 import MapView from "esri/views/MapView";
 
@@ -32,7 +31,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   private _center: Array<number> = [0.1278, 51.5074];
   private _basemap = 'streets';
   private _loaded = false;
-  private _view: esri.MapView = null; 
+  private _view: MapView = null; 
 
   get mapLoaded(): boolean {
     return this._loaded;
@@ -70,14 +69,14 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   async initializeMap() {
 
       // Configure the Map
-      const mapProperties: esri.MapProperties = {
+      const mapProperties = {
         basemap: this._basemap
       };
 
       const map = new Map(mapProperties);
 
       // Initialize the MapView
-      const mapViewProperties: esri.MapViewProperties = {
+      const mapViewProperties = {
         container: this.mapViewEl.nativeElement,
         center: this._center,
         zoom: this._zoom,
