6916a5cce5cf8b39dd3900c0c47eb62073cb0995
Andy G <agup2001@msn.com>
2020-10-02 16:04:09 -0600

    Fix master branch

--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -1,5 +1,5 @@
 /*
-  Copyright 2020 Esri
+  Copyright 2019 Esri
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
@@ -11,29 +11,41 @@
   limitations under the License.
 */
 
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Input, Output, EventEmitter, NgZone } from '@angular/core';
-import Map from "esri/Map";
-import MapView from "esri/views/MapView";
-import FeatureLayer from "esri/layers/FeatureLayer";
-import esriConfig from "esri/config";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy
+} from "@angular/core";
+import { loadModules } from "esri-loader";
+import esri = __esri; // Esri TypeScript Types
 
 @Component({
-  selector: 'app-esri-map',
-  templateUrl: './esri-map.component.html',
-  styleUrls: ['./esri-map.component.scss']
+  selector: "app-esri-map",
+  templateUrl: "./esri-map.component.html",
+  styleUrls: ["./esri-map.component.scss"]
 })
 export class EsriMapComponent implements OnInit, OnDestroy {
-
   @Output() mapLoadedEvent = new EventEmitter<boolean>();
 
   // The <div> where we will place the map
-  @ViewChild('mapViewNode', { static: true }) private mapViewEl: ElementRef;
+  @ViewChild("mapViewNode", { static: true }) private mapViewEl: ElementRef;
 
+  /**
+   * _zoom sets map zoom
+   * _center sets map center
+   * _basemap sets type of map
+   * _loaded provides map loaded status
+   */
   private _zoom = 10;
   private _center: Array<number> = [0.1278, 51.5074];
-  private _basemap = 'streets';
+  private _basemap = "streets";
   private _loaded = false;
-  private _view: MapView = null; 
+  private _view: esri.MapView = null;
 
   get mapLoaded(): boolean {
     return this._loaded;
@@ -66,109 +78,52 @@ export class EsriMapComponent implements OnInit, OnDestroy {
     return this._basemap;
   }
 
-  constructor(private zone: NgZone) { }
+  constructor() {}
 
   async initializeMap() {
+    try {
+      // Load the modules for the ArcGIS API for JavaScript
+      const [EsriMap, EsriMapView] = await loadModules([
+        "esri/Map",
+        "esri/views/MapView"
+      ]);
 
-    this.initializeWorkers();
+      // Configure the Map
+      const mapProperties: esri.MapProperties = {
+        basemap: this._basemap
+      };
 
-    const citiesRenderer = {
-      type: "simple", // autocasts as new SimpleRenderer()
-      symbol: {
-        type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-        size: 5,
-        color: [0, 244, 255, 1]
-      }
-    };
+      const map: esri.Map = new EsriMap(mapProperties);
 
-    const policeStopsLayer = new FeatureLayer({
-      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Denver_Police_Stops/FeatureServer/0",
-      renderer: citiesRenderer as any
-    });
+      // Initialize the MapView
+      const mapViewProperties: esri.MapViewProperties = {
+        container: this.mapViewEl.nativeElement,
+        center: this._center,
+        zoom: this._zoom,
+        map: map
+      };
 
-    // Configure the Map
-    const mapProperties = {
-      basemap: this._basemap,
-      layers: [policeStopsLayer]
-    };
-
-    const map = new Map(mapProperties);   
-
-    // Initialize the MapView
-    const mapViewProperties = {
-      container: this.mapViewEl.nativeElement,
-      center: this._center,
-      zoom: this._zoom,
-      map: map
-    };
-
-    this._view = new MapView(mapViewProperties);
-    
-    // wait for the map to load
-    await this._view.when(); 
-    return this._view;
-  }
-
-  initializeWorkers() {
-    const DEFAULT_WORKER_URL = "https://js.arcgis.com/4.16/";
-    const DEFAULT_LOADER_URL = `${DEFAULT_WORKER_URL}dojo/dojo-lite.js`;
-
-    esriConfig.workers.loaderUrl = DEFAULT_LOADER_URL;
-    esriConfig.workers.loaderConfig = {
-      baseUrl: `${DEFAULT_WORKER_URL}dojo`,
-      packages: [
-        { name: "esri", location: `${DEFAULT_WORKER_URL}esri` },
-        { name: "dojo", location: `${DEFAULT_WORKER_URL}dojo` },
-        { name: "dojox", location: `${DEFAULT_WORKER_URL}dojox` },
-        { name: "dstore", location: `${DEFAULT_WORKER_URL}dstore` },
-        { name: "moment", location: `${DEFAULT_WORKER_URL}moment` },
-        { name: "@dojo", location: `${DEFAULT_WORKER_URL}@dojo` },
-        {
-          name: "cldrjs",
-          location: `${DEFAULT_WORKER_URL}cldrjs`,
-          main: "dist/cldr"
-        },
-        {
-          name: "globalize",
-          location: `${DEFAULT_WORKER_URL}globalize`,
-          main: "dist/globalize"
-        },
-        {
-          name: "maquette",
-          location: `${DEFAULT_WORKER_URL}maquette`,
-          main: "dist/maquette.umd"
-        },
-        {
-          name: "maquette-css-transitions",
-          location: `${DEFAULT_WORKER_URL}maquette-css-transitions`,
-          main: "dist/maquette-css-transitions.umd"
-        },
-        {
-          name: "maquette-jsx",
-          location: `${DEFAULT_WORKER_URL}maquette-jsx`,
-          main: "dist/maquette-jsx.umd"
-        },
-        { name: "tslib", location: `${DEFAULT_WORKER_URL}tslib`, main: "tslib" }
-      ]
-    };    
+      this._view = new EsriMapView(mapViewProperties);
+      await this._view.when();
+      return this._view;
+    } catch (error) {
+      console.log("EsriLoader: ", error);
+    }
   }
 
   ngOnInit() {
-    // For more info on change detection handling see: https://github.com/Esri/angular-cli-esri-map/issues/70
-    // as well as this blog post: https://www.andygup.net/manual-change-detection-in-angular-for-performance/
-    this.zone.runOutsideAngular(() => {
-      // Initialize MapView and return an instance of MapView
-      this.initializeMap().then((mapView) => {
-        // The map has been initialized
-        console.log('mapView ready: ', mapView.ready);
-        this._loaded = mapView.ready;
-        this.mapLoadedEvent.emit(true);
-      });
-    })
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap().then(mapView => {
+      // The map has been initialized
+      console.log("mapView ready: ", this._view.ready);
+      this._loaded = this._view.ready;
+      this.mapLoadedEvent.emit(true);
+    });
   }
 
   ngOnDestroy() {
-    if (this._view){
+    if (this._view) {
+      // destroy the map view
       this._view.container = null;
     }
   }
