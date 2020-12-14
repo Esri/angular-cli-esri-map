60e222093b53a9c6e45b652ea8a6387beb515b54
Matt Callahan <matthew.callahan@outlook.com>
2020-12-14 09:10:00 -0700

    remove hardcoded css url; add options param to loadModules()

--- src/app/esri-loader.service.ts
+++ src/app/esri-loader.service.ts
@@ -5,10 +5,11 @@ import { loadModules } from "esri-loader";
   providedIn: "root",
 })
 export class EsriLoaderService {
+  apiOptions = { version: "4.17", css: true };
   constructor() {}
 
   public async loadModules(modules: string[]): Promise<any[]> {
-    return await loadModules(modules);
+    return await loadModules(modules, this.apiOptions);
   }
 
   public getInstance<T>(type: new (paramObj: any) => T, paramObj?: any): T {
