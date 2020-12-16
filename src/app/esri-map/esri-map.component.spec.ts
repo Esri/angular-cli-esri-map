eba1fc6a3601fc02d8d103d8c2b3bf1aea10a828
Andy G <agup2001@msn.com>
2020-12-16 15:51:42 -0700

    Update esri-loader sample to Angular 11, esri-loader 4.17.0

--- src/app/esri-map/esri-map.component.spec.ts
+++ src/app/esri-map/esri-map.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EsriMapComponent } from './esri-map.component';
 
@@ -7,7 +7,7 @@ describe('EsriMapComponent', () => {
   let fixture: ComponentFixture<EsriMapComponent>;
   let app: any; // debugElement.componentInstance
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [EsriMapComponent]
     })
